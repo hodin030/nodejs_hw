5ca9a3b079a834eaf74d5853a9812ed2be2eb1e5
hodin <hodin030@gmail.com>
2023-09-03 23:16:18 +0900

    route 수정

--- src/routes/book-route.js
+++ src/routes/book-route.js
@@ -7,6 +7,6 @@ router.get('/:bookNo', BookController.findBookByBookNo);
 router.post('/', BookController.registNewBook);
 
 /* 함수 호출 시 매개변수 입력할 필요 없는 이유: 
-    router에서 알아서 기본 매개변수 (req, res, next) 생성해준다!!! */
+    router에서 알아서 기본 매개변수 (req, res, next) 생성해준다!! */
 
 module.exports = router;
